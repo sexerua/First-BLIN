135a824cb39fded2a56e8edc1cf8e13e7481b118
Vladislav Korostel <korostel.v.s@gmail.com>
2014-01-23 20:47:36 +0200

    Обновил Book.java
    Добавил поля(переменные) в Book.java

--- Book.java
+++ Book.java
@@ -1,9 +1,16 @@
 package library;
 
 public class Book {
-  String name;
+  private String name;             //Название книги
+  private String author;           //Автор книги
+  private int year;                //Год издательства книги
+  private boolean chosen = false;  //Является ли избранным
+  
+  public String[] chapters;        //Список глав книги
   
   Book(){
     
   }
+  
+  //Далее пойдут геттеры и сеттеры
 }
