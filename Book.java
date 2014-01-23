be9256f5e7487d617b6299be95cd14c9602b28d7
dimonKiev <platinymman@gmail.com>
2014-01-24 01:09:53 +0200

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,23 +1,78 @@
 package library;
 
-public class Book {
-  private String name;             //Название книги
-  private String author;           //Автор книги
-  private int year;                //Год издательства книги
-  private boolean chosen = false;  //Является ли избранным
-  
-  public String[] chapters;        //Список глав книги
-  
-  Book(){
-    
-  }
-  
-  //Далее пойдут геттеры и сеттеры
-  String getName(){            // получить название книги
-    return name;
-  }
-  
-  void setName(String name){   // записать название книги
-    this.name = name;
-  }  
+public class Book
+{
+
+	private String name;                 //Название книги
+	private String author;              //Автор книги
+	private String [] chapters;        //Список глав книги
+	private int year;                 //Год издательства книги
+	private boolean chosen = false;  //Является ли избранным
+	
+	
+	// КОНСТРУКТОРЫ НА ВЫБОР
+	public Book(String name,String...chapters){
+		this.name = name;
+		this.chapters = chapters;
+		this.author = "";
+		this.year = 0000;
+		this.chosen = false;
+	}
+	public Book(String name, int year,String...chapters){
+		this.name = name;
+		this.chapters = chapters;
+		this.author = "";
+		this.year = year;
+		this.chosen = false;
+	}
+	public Book(String name, int year, String author, String...chapters){
+		this.name = name;
+		this.chapters = chapters;
+		this.author = author;
+		this.year = year;
+		this.chosen = false;
+	}
+	
+	//МЕТОДЫ : СЕТТЕРЫ И ГЕТТЕРЫ
+	public String getName()             // получить название книги
+	{
+		return name;
+	}
+	public void setName(String name)   // записать название книги
+	{
+		this.name = name;
+	}
+	public String getAuthor()         // получить название
+	{
+		return author;
+	}
+	public void setAuthor(String author)   // записать название
+	{
+		this.author = author;
+	}
+	public String[] getChapters()    // получить название
+	{
+		return chapters;
+	}
+	public void setChapters(String[] chapters)    // записать название
+	{
+		this.chapters = chapters;
+	}
+	public int getYear()            // получить название
+	{
+		return year;
+	}
+	public void setYear(int year)    // записать название
+	{
+		this.year = year;
+	}
+	public boolean isChosen()      // получить название
+	{
+		return chosen;
+	}
+	public void setChosen(boolean chosen)   // записать название
+	{
+		this.chosen = chosen;
+	}
+	
 }
