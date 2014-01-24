e861d1fb7955e00d7f8791a338dcccff11ba37a6
dimonKiev <platinymman@gmail.com>
2014-01-24 20:11:39 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,19 +8,14 @@ public class Main
 		// РЕАЛИЗАЦИЯ МЕТОДА № 1 ДЛЯ ПРОВЕРКИ РАБОТОСПОСОБНОСТИ МЕТОДА (добавить книгу в библиотеку)
 		Lib lib = new Lib(5);
 		
-		lib.addBook("b1");
+	        lib.addBook("b1");
 		lib.addBook("b2",1995);
 		lib.addBook("b3", 2005, "Dima");
 		lib.addBook("b4", 2009, "Dmytro", "g1","g2","g3");
 		
-		System.out.println(lib.books[0].getName());
-		System.out.println(lib.books[1].getName()+"  "+lib.books[1].getYear());
-		System.out.println(lib.books[2].getName()+"  "+lib.books[2].getYear()+" "+lib.books[2].getAuthor());
-		System.out.println(lib.books[3].getName()+"  "+lib.books[3].getYear()+" "+lib.books[3].getAuthor());
-		
-		for(String item :lib.books[3].getChapters()){
-			System.out.println(item+" ");
-		}
+		lib.printAllBooks();
+		lib.format(1);
+		lib.printAllBooks();
 	}
 
 }
