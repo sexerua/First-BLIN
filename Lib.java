79e4f312220b648301c94d4fb2d2d3f30ec1f063
dimonKiev <platinymman@gmail.com>
2014-01-24 17:11:57 +0200

    Update Lib.java

--- Lib.java
+++ Lib.java
@@ -58,5 +58,33 @@ public class Lib
 		  }
 		  System.out.println("КНИГА "+bookName+" УСПЕШНО ДОБАВЛЕНА В БИБЛИОТЕКУ");
 	  }
+	  
+	  
+	  //МЕТОД ПРОСМОТР, который выводит инфо о книгах в библиотеке
+	  
+	  public void printAllBooks(){
+		  int counter=1;
+		  
+		  for(int i=0; i<books.length; i++){
+			 if(books[i]!=null){
+				  System.out.println(" НАЗВАНИЕ КНИГИ : "+books[i].getName()+
+						             " || ГОД КНИГИ : "+books[i].getYear()+
+						             " || АВТОР КНИГИ : "+books[i].getAuthor());
+				  System.out.println(" СОДЕРЖАНИЕ КНИГИ :");
+				  
+				  if(books[i].getChapters()!=null){
+				  for(String item :books[i].getChapters()){
+						System.out.println((counter++)+"."+item);
+					}
+				  
+			 }else{System.out.println("В КНИГЕ НЕТ СОДЕРЖАНИЯ ГЛАВ");
+				   System.out.println();
+				  }
+				  
+			  }else{break;}
+		  }
+	  }
+	  //---------------------------------------------------------------
+	  
 }
 
