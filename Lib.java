1250fc76f4cd32de573f146990ad6aed281ba387
dimonKiev <platinymman@gmail.com>
2014-01-24 20:03:32 +0200

    Update Lib.java

--- Lib.java
+++ Lib.java
@@ -60,30 +60,61 @@ public class Lib
 	  }
 	  
 	  
-	  //МЕТОД ПРОСМОТР, который выводит инфо о книгах в библиотеке
+	  //МЕТОД № 2 ПРОСМОТР, который выводит инфо о книгах в библиотеке
 	  
 	  public void printAllBooks(){
-		  int counter=1;
+				 
+		 if (foolFormat==false){printInfo(0);}else{printInfo(1);}
+	  }
+	  
+	  // ПОМОЩНИК МЕТОДА ПРОСМОТР, который выводит инфо о книгах в библиотеке  0--краткий формат; 1--полный формат
+	  private void printInfo(int printFormat){
+		  int counter=1;// СЧЕТЧИК ДЛЯ ГЛАВ (ДЕЛАЕТ НОМЕРАЦИЮ ГЛАВ)
+		 
+		  switch(printFormat){
 		  
-		  for(int i=0; i<books.length; i++){
-			 if(books[i]!=null){
-				  System.out.println(" НАЗВАНИЕ КНИГИ : "+books[i].getName()+
-						             " || ГОД КНИГИ : "+books[i].getYear()+
-						             " || АВТОР КНИГИ : "+books[i].getAuthor());
-				  System.out.println(" СОДЕРЖАНИЕ КНИГИ :");
-				  
-				  if(books[i].getChapters()!=null){
-				        for(String item :books[i].getChapters()){
-				            System.out.println((counter++)+"."+item);
-					}
-				  
-			          }else{System.out.println("В КНИГЕ НЕТ СОДЕРЖАНИЯ ГЛАВ");
-				        System.out.println();
-				       }
-				  
-	                 }else{break;}
+		  case 0:
+			  for(int i=0; i<books.length; i++){
+				  if(books[i]!=null){
+					  System.out.println(" НАЗВАНИЕ КНИГИ : "+books[i].getName()+
+							             " || ГОД КНИГИ : "+books[i].getYear()+
+							             " || АВТОР КНИГИ : "+books[i].getAuthor());
+				  }
+			  }
+			  break;
+		 
+		  
+		  case 1:
+			  for(int i=0; i<books.length; i++){
+					 if(books[i]!=null){
+						  System.out.println(" НАЗВАНИЕ КНИГИ : "+books[i].getName()+
+								             " || ГОД КНИГИ : "+books[i].getYear()+
+								             " || АВТОР КНИГИ : "+books[i].getAuthor());
+						  System.out.println(" СОДЕРЖАНИЕ КНИГИ :");
+						  
+						  if(books[i].getChapters()!=null){
+						  for(String item :books[i].getChapters()){
+								System.out.println((counter++)+"."+item);
+							}
+						  
+					 }else{System.out.println("В КНИГЕ НЕТ СОДЕРЖАНИЯ ГЛАВ");
+						   System.out.println();
+						  }
+						  
+					  }else{break;}
+				  }
+			  break;
 		  }
 	  }
+	  //---------------------------------------------------------------
+	  
+	   // МЕТОД №9 ФОРМАТ, можно и yf булевский вариант переделать , int bookFormat  можно поменять на boolean bookFormat 
+	   // зависит от кода класса main
+	  public void format(int bookFormat){
+		  foolFormat = (bookFormat==0)? false:true;
+		  
+	  }
+	  
 	  //---------------------------------------------------------------
 	  
 }
