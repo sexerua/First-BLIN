429c27dd5bec47197d460f6caca686b2f389d424
dimonKiev <platinymman@gmail.com>
2014-01-24 03:12:13 +0200

    Update Lib.java

--- Lib.java
+++ Lib.java
@@ -9,5 +9,53 @@ public class Lib
 	  }
 	  
 	  // ДАЛЕЕ ВСТАВЛЯЙТЕ СВОИ МЕТОДЫ И ПРОБУЙТЕ ЗАПУСТИТЬ СВОЙ МЕТОД ЧЕРЕЗ Main СОЗДАВ ЭКЗЕМПЛЯР КЛАССА Lib
+	  
+	  
+	  
+	  
+	  
+	  // БАЗОВЫЙ МЕТОД ДОБАВИТЬ КНИГУ В БИБЛИОТЕКУ
+	  public void addBook (String bookName){
+		  for(int i=0; i<books.length; i++){
+			  if(books[i]==null){
+				  books[i] = new Book(bookName);
+				  break;
+			  }
+		  }
+		  System.out.println("КНИГА "+bookName+" УСПЕШНО ДОБАВЛЕНА В БИБЛИОТЕКУ");
+	  }
+	  
+	  // ПЕРЕГРУЗКА БАЗОВОГО МЕТОДА ДОБАВИТЬ КНИГУ В БИБЛИОТЕКУ
+	  public void addBook (String bookName, int bookYear){
+		  for(int i=0; i<books.length; i++){
+			  if(books[i]==null){
+				  books[i] = new Book(bookName,bookYear);
+				  break;
+			  }
+		  }
+		  System.out.println("КНИГА "+bookName+" УСПЕШНО ДОБАВЛЕНА В БИБЛИОТЕКУ");
+	  }
+	  
+	  // ПЕРЕГРУЗКА БАЗОВОГО МЕТОДА ДОБАВИТЬ КНИГУ В БИБЛИОТЕКУ
+	  public void addBook (String bookName, int bookYear, String bookAuthor){
+		  for(int i=0; i<books.length; i++){
+			  if(books[i]==null){
+				  books[i] = new Book(bookName,bookYear,bookAuthor);
+				  break;
+			  }
+		  }
+		  System.out.println("КНИГА "+bookName+" УСПЕШНО ДОБАВЛЕНА В БИБЛИОТЕКУ");
+	  }
+	  
+	  //ПЕРЕГРУЗКА БАЗОВОГО МЕТОДА ДОБАВИТЬ КНИГУ В БИБЛИОТЕКУ
+	  public void addBook (String bookName, int bookYear, String bookAuthor, String...bookChapters){
+		  for(int i=0; i<books.length; i++){
+			  if(books[i]==null){
+				  books[i] = new Book(bookName,bookYear,bookAuthor,bookChapters);
+				  break;
+			  }
+		  }
+		  System.out.println("КНИГА "+bookName+" УСПЕШНО ДОБАВЛЕНА В БИБЛИОТЕКУ");
+	  }
 }
 
