6d5d2912a0d96536578f3d34c0f1d7dd0d2eb132
VasilenkoDenis <vasilenko397@gmail.com>
2014-01-29 22:33:38 +0200

    Заменил метод сортировки на метод поиска книги по году издания

--- Project/src/library/Lib.java
+++ Project/src/library/Lib.java
@@ -134,29 +134,62 @@ public class Lib
 
 	  //---------------------------------------------------------------
 	  
-	  //Метод № 4  Поиск книг по году издания
-	  public void sortByYear(){
+            
+      // Метод № 4 ПОИСК КНИГИ В БИБЛИОТЕКЕ ПО ГОДУ ИЗДАНИЯ
+      public void findByYear(int findYear){
 
-		// Цикл по книгам всем библиотеки
-	   	 for(int i=0; i<books.length-1;i++){
-        		
-        		// Проверка на конец книг в библиотеке         		 
-        		if (books[i+1] == null)
-        			return;       		
-        		// Сравнение         		 
-        		if (books[i].getYear() > books[i+1].getYear()){
-            		Book book = new Book("");      // Сохраняем в промежуточный объект      	         			
-            		 	// Меняем элементы массивов
-        			book = books[i];    
-				books[i] = books[i+1];
-        			books[i+1] = book;
-        			// Идём на рекурсию 
-        			sortByYear();
-        		}
-        	 }        	 
- 	
-		 
-		}
+          	int counter = 0;
+          	int chapter = 1;
+             
+          	System.out.println(" РЕЗУЛЬТАТЫ ПОИСКА:");
+          		for (Book item: books){
+                   
+          	 if (item == null)                 // Если добрались до пустого элемента, то дальше в библиотеке книг нет
+          		 break;
+         	 	 
+          	 if (item.getYear() == findYear){
+                
+          		 counter++;
+                
+          		 System.out.println("--------------- КНИГА № " + counter + ": ---------------");
+             
+          		 switch((fullFormat)? 1:0){
+       
+          		  case 0:
+         		  			System.out.println(" АВТОР КНИГИ : "+item.getAuthor()+"\n" +
+         		  								" || НАЗВАНИЕ КНИГИ : "+item.getName()+"\n" +
+                              					" || ГОД КНИГИ : "+item.getYear()+"\n");
+                               
+               
+         	 			  	break;
+      
+          		  default:
+                       		System.out.println(" НАЗВАНИЕ КНИГИ : "+item.getName()+
+                           						" || ГОД КНИГИ : "+item.getYear()+
+                           						" || АВТОР КНИГИ : "+item.getAuthor());
+                       		System.out.println(" СОДЕРЖАНИЕ КНИГИ :");
+                                       
+                       		if(item.getChapters()!=null){
+                                     
+                                    for(String chapName :item.getChapters()){
+                                                     System.out.println((chapter++)+"."+chapName);
+                                     }
+                                       
+                             }
+                             else{
+                                     System.out.println("В КНИГЕ НЕТ СОДЕРЖАНИЯ ГЛАВ");
+                             }
+                             
+
+         	 		 } //switch-case
+         	 	 System.out.println();
+         	 	 } // if
+          } // for
+
+          if (counter == 0)
+         	 System.out.println("НИЧЕГО НЕ НАЙДЕНО!\n");
+       
+          }
 
 	  //---------------------------------------------------------------
 	  
